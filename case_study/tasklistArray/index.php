e455b75313ababc29e2b739e7e6454d42d143a5e
DamianoGeuna <damigeuna@gmail.com>
2021-03-08 10:24:29 +0100

    versione HTML del layout

--- case_study/tasklistArray/index.php
+++ case_study/tasklistArray/index.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task List</title>
+    <link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+    <form action="">
+        <input type="text" name="searchText">
+        <button type="submit">cerca</button>
+    </form>
+    <ul>
+        <li class="tasklist-item tasklist-item-progress">latte <b>progress</b></li>
+        <li class="tasklist-item tasklist-item-done">uova <b>done</b></li>
+        <li class="tasklist-item tasklist-item-todo">caffè <b>todo</b></li>
+    </ul>
+</body>
+</html>
